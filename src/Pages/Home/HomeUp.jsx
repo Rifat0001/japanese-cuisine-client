32103ab251e361e416fd313e7a0d77ee2ce33670
Rifat0001 <mahmudrifat764@gmail.com>
2023-05-28 00:28:43 +0600

    home page complete

--- src/Pages/Home/HomeUp.jsx
+++ src/Pages/Home/HomeUp.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './HomeUp.css'
 import SingleChef from '../SingleChef/SingleChef';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 const HomeUp = () => {
     const [chefs, setChefs] = useState([]);
     useEffect(() => {
@@ -21,16 +22,16 @@ const HomeUp = () => {
                 </div>
             </section>
             <section className='flex justify-between items-center  py-20'>
-                <div className='w-7/12 me-20'>
+                <div className='w-1/2 me-20'>
                     <h1 className="text-4xl text-primary font-bold">About Us</h1>
                     <p className="py-6 text-black">Japanese cuisine is renowned for its delicate flavors, exquisite presentation, and emphasis on fresh, high-quality ingredients. It reflects a harmonious blend of tradition and innovation, rooted in centuries of culinary artistry. Rice, seafood, vegetables, and soy-based products form the foundation of Japanese cuisine.</p>
                     <button className="btn btn-primary">Read More</button>
                 </div>
-                <div className='w-5/12'>
+                <div className='w-1/2'>
                     <img src="https://i.ibb.co/Fxh2dT1/cccc.jpg" className='rounded-md' alt="" />
                 </div>
             </section>
-            <section className=''>
+            <section className='my-8'>
                 <h1 className="text-4xl text-primary font-bold text-center mb-4">Our Chefs</h1>
                 <div className='grid grid-cols-3'>
                     {
@@ -41,7 +42,42 @@ const HomeUp = () => {
                 </div>
 
             </section>
-        </div>
+            <section className='flex justify-between items-center  py-20'>
+                <div className='w-1/2 me-20'>
+                    <h1 className="text-4xl text-primary font-bold">Contact Us</h1>
+                    <p className="py-6 text-black">Have a question, feedback, or need assistance? Our Contact Us section is designed to make it easy for you to reach out and get the support you need. Our dedicated support team is here to assist you with any inquiries or concerns you may have.</p>
+                    <div className='flex gap-4'>
+                        <FaFacebook className='text-3xl'></FaFacebook>
+                        <FaInstagram className='text-3xl'></FaInstagram>
+                        <FaTwitter className='text-3xl'></FaTwitter>
+                        <FaLinkedin className='text-3xl'></FaLinkedin>
+                    </div>
+
+
+                </div>
+                <div className='w-1/2 border  shadow-xl drop-shadow-2xl bg-base-100 px-4 py-4 rounded-md'>
+                    <div className="form-control ">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="Your Email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Message</span>
+                        </label>
+                        <input type="text" placeholder="Your Message" className="input input-bordered" />
+                        <label className="label">
+
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Send Message</button>
+                    </div>
+                </div>
+
+            </section >
+        </div >
     );
 };
 
