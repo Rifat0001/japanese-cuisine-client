b48e1feabf6b1fe30ceca3b465e31a538c0e4e02
Rifat0001 <mahmudrifat764@gmail.com>
2023-05-28 23:29:32 +0600

    env

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBa9dEjGa92xySejLPjswaDMb5lFqPJUHM",
-    authDomain: "japanese-cuisine-aad03.firebaseapp.com",
-    projectId: "japanese-cuisine-aad03",
-    storageBucket: "japanese-cuisine-aad03.appspot.com",
-    messagingSenderId: "962848699880",
-    appId: "1:962848699880:web:557ab5edaff833e40e605d"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
